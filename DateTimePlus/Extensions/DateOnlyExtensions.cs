72792ee58ca016912cf38dfc75c5cef45f5cb1e0
Álvaro Vian <alvarovian95@gmail.com>
2026-01-14 11:08:53 +0100

    Initial commit: DateTimePlus library with .slnx

---
+++ DateTimePlus/Extensions/DateOnlyExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimePlus.Extensions
+{
+    internal class DateOnlyExtensions
+    {
+    }
+}
